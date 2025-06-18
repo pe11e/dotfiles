f47370b2d8fa6493f2d77c0940f78407e0ab0df1
Per Schold <persc@axis.com>
2025-06-18 10:41:41 +0200

    change emacs config

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -2,3 +2,18 @@
   (expand-file-name
    "config.org"
    user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-log-types
+   '(((elpaca-use-package-compat))
+     ((elpaca-use-package-compat))
+     ((elpaca-use-package-compat)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
